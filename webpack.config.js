5fc904ff2a94296a4ed66239acb6a2c758e2aed4
wingtonbrito <wingtonbrito@gmail.com>
2017-10-25 00:24:47 -0600

    Webpack tuning

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,24 @@
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack'),
+    path = require('path'),
+    fs = require('fs')
 
-module.exports = {
+const packages = require('./package.json');
+
+let config = {
   entry: {
+    'javascripts/vendor-bundle.js': Object.keys(packages.dependencies),
     'javascripts/site-bundle.js': './clients/react/app.js'
   },
   output: {
-    path: __dirname + '/public/',
+    path: path.resolve(__dirname + '/public/'),
     filename: '[name]'
   },
+  // watch: true,
+  // devServer: {
+  //   contentBase: __dirname + '/public/',
+  //   filename: "[name]",
+  // },
   module: {
     loaders: [
       {
@@ -38,6 +49,47 @@ module.exports = {
     ]
   },
   plugins: [
-    new ExtractTextPlugin("stylesheets/style.css")
+    new webpack.optimize.CommonsChunkPlugin({
+       name: ['javascripts/vendor-bundle.js'],
+       minChunks: Infinity
+    }),
+    new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+    }),
+    new ExtractTextPlugin("stylesheets/style.css",{
+      allChunks: true
+    }),
+    new webpack.DefinePlugin({
+        'process.env': {
+            'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        }
+    }),
   ]
 };
+
+if (process.env.NODE_ENV === 'production'){
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            screw_ie8: true
+        }
+    })
+  )
+}
+else {
+    config.devtool = "#cheap-module-source-map";
+    config.watch = true;
+    config.devServer = {
+        contentBase: path.resolve(__dirname + '/public/'),
+        hot: true,
+        inline: true,
+        host: "0.0.0.0",
+        port: 2708
+    }
+    config.plugins.push(
+        new webpack.HotModuleReplacementPlugin()
+    )
+}
+
+module.exports = config
